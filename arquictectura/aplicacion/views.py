21a17e82cbc80a97dabc5c17ec91f3d3ad167ecd
JAOC1930 <jhoelordonez2@gmail.com>
2024-01-22 00:06:32 -0500

    Cambios perfectos 2

--- arquictectura/aplicacion/views.py
+++ arquictectura/aplicacion/views.py
@@ -81,30 +81,24 @@ def retirarProductoMalE():
 class InventarioViewSet(viewsets.ModelViewSet):
     queryset = Inventario.objects.all()
     serializer_class = InventarioSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class AlmacenViewSet(viewsets.ModelViewSet):
     queryset = Almacen.objects.all()
     serializer_class = AlmacenSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class RolPersonaViewSet(viewsets.ModelViewSet):
     queryset = RolPersona.objects.all()
     serializer_class = RolPersonaSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class PersonalViewSet(viewsets.ModelViewSet):
     queryset = Personal.objects.all()
     serializer_class = PersonalSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class CategoriaProductoViewSet(viewsets.ModelViewSet):
     queryset = CategoriaProducto.objects.all()
     serializer_class = CategoriaProductoSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
